a6a12e6cd962e50cf09f8eb6ca058d1f0cc9d987
serhankunt <hserhan006@gmail.com>
2024-04-14 19:59:07 +0300

    Add project files.

---
+++ Context/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Cache.WebAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cache.WebAPI.Context;
+
+public sealed class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions options) : base(options)
+    {
+    }
+    public DbSet<Product> Products { get; set; }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace Cache.WebAPI.Models;
+
+public sealed class Product
+{
+    public Product()
+    {
+        Id = Guid.NewGuid();    
+    }
+    public Guid Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+
+}
